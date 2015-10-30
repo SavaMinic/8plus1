deb4e899d251858c563f840183252934653e9ec4
savam <savam@nordeus.eu>
2015-10-30 22:28:22 +0100

    Removed unused staff from Water Flow, added lillies to scene

---
+++ Assets/Scripts/Frog.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Frog : MonoBehaviour
+{
+
+	private Animator frogAnimator;
+
+	void Awake()
+	{
+		frogAnimator = GetComponent<Animator>();
+		frogAnimator.speed = 0f;
+	}
+}
